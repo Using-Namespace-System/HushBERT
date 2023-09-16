97e07085a2ac1ca2b2f8cde026ea1a81282422d0
Using-Namespace-System <RiverScriptTechServices@gmail.com>
2023-09-16 16:10:14 -0400

    Package Properties
    Package properties created

---
+++ HushBERT/__init__.py
@@ -0,0 +1,7 @@
+# HushBERT/__init__.py
+
+"""HushBERT - A Python package exploring options and tools for noise reduction of BERTopic models."""
+
+__version__ = "0.1.0"
+__author__ = "Brian Recktenwall-Calvet"
+__license__ = "MIT License"
